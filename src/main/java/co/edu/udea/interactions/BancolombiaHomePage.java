4d6917f1e2b778ed5c54f03efa875d07d7889b33
Diego Alejandro Poveda <alejopov@gmail.com>
2023-05-06 18:52:06 -0500

    fix some code smells

--- src/main/java/co/edu/udea/interactions/BancolombiaHomePage.java
+++ src/main/java/co/edu/udea/interactions/BancolombiaHomePage.java
@@ -7,44 +7,37 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import org.openqa.selenium.Keys;
 
+import java.security.SecureRandom;
+
 import static co.edu.udea.userinterfaces.BancolombiaPage.*;
 
 public class BancolombiaHomePage implements Interaction {
     @Override
     public <T extends Actor> void performAs(T actor) {
-        // TODO: click en Conoce más de nuestros productos > Saber más
+        SecureRandom random = new SecureRandom();
+
         actor.attemptsTo(Click.on(KNOW_MORE_CREDIT_BUTTON));
 
-        // TODO: click en Crédito de libre inversión Bancolombia > Simular
         actor.attemptsTo(Click.on(SIMULATE_CREDIT_BUTTON));
 
-        // TODO: click en Continuar
         actor.attemptsTo(Click.on(CONTINUE_CREDIT_BUTTON));
 
-        // TODO: click en Si
         actor.attemptsTo(Click.on(YES_RADIO));
 
-        // TODO: escribir un numero entre 1.000.000 y 500.000.000 y dar Enter
-        int randomAmount = (int) (Math.random() * (500000000 - 1000000 + 1)) + 1000000;
+        int randomAmount = (random.nextInt() * (500000000 - 1000000 + 1)) + 1000000;
         actor.attemptsTo(Enter.theValue("" + randomAmount).into(AMOUNT_INPUT).thenHit(Keys.RETURN));
 
-        int randomMonths = (int) (Math.random() * (84 - 48 + 1)) + 48;
-        // TODO: escribir numero entre 48 y 84 en ¿A cuantos meses? y dar Enter
+        int randomMonths = (random.nextInt() * (84 - 48 + 1)) + 48;
         actor.attemptsTo(Enter.theValue("" + randomMonths).into(MONTHS_INPUT).thenHit(Keys.RETURN));
 
-        // TODO: click en Calendario
         actor.attemptsTo(Click.on(CALENDAR_INPUT));
 
-        // TODO: click en un año
         actor.attemptsTo(Click.on(CALENDAR_YEAR_BUTTON));
 
-        // TODO: click en un mes
         actor.attemptsTo(Click.on(CALENDAR_MONTH_BUTTON));
 
-        // TODO: click en un dia
         actor.attemptsTo(Click.on(CALENDAR_DAY_BUTTON));
 
-        // TODO: Click en simular
         actor.attemptsTo(Click.on(SIMULATE_BUTTON));
     }
 
