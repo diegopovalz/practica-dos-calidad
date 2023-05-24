6e67fa9000431381dcd425e3c896008181dd8357
Diego Alejandro Poveda <alejopov@gmail.com>
2023-05-23 20:12:53 -0500

    add missing imports

--- src/main/java/co/edu/udea/interactions/BancolombiaHomePage.java
+++ src/main/java/co/edu/udea/interactions/BancolombiaHomePage.java
@@ -8,6 +8,8 @@ import net.serenitybdd.screenplay.actions.Enter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
