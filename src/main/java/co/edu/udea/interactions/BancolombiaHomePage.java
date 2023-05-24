3a820214cff1e95b8a47f4be73a42f48863d1042
valeria <valeria.granada@evertecinc.com>
2023-05-23 20:18:52 -0500

    fix: change condition

--- src/main/java/co/edu/udea/interactions/BancolombiaHomePage.java
+++ src/main/java/co/edu/udea/interactions/BancolombiaHomePage.java
@@ -31,13 +31,12 @@ public class BancolombiaHomePage implements Interaction {
         SecureRandom random = new SecureRandom();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 
-        ExpectedCondition<WebElement> elementClickable = ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='closeModalBtn']"));
+        WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='closeModalBtn']")));
 
-        if (elementClickable.apply(driver) != null) {
-            WebElement closeButton = wait.until(elementClickable);
+        if (closeButton != null) {
             closeButton.click();
         }
-        
+
         actor.attemptsTo(Click.on(KNOW_MORE_CREDIT_BUTTON));
 
         actor.attemptsTo(Click.on(SIMULATE_CREDIT_BUTTON));
